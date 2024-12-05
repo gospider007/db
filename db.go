b6c4b1698eb1042ac35111bda1e9d42591d5691e
gospider <2216403312@qq.com>
2024-12-05 17:01:39 +0800

    sync

--- db.go
+++ db.go
@@ -1,81 +1,86 @@
 package db
 
 import (
-	"bytes"
-	"encoding/gob"
+	"context"
+	"sync"
 	"time"
-
-	badger "github.com/dgraph-io/badger/v4"
-	"github.com/gospider007/tools"
 )
 
 type ClientOption struct {
-	Dir string
-	Ttl time.Duration
+	TTL time.Duration
 }
 
-func NewClient(option ClientOption) (*Client, error) {
-	opt := badger.DefaultOptions(option.Dir)
-	if option.Dir == "" {
-		opt = opt.WithInMemory(true)
+func NewClient(ctx context.Context, option ClientOption) *Client {
+	if ctx == nil {
+		ctx = context.TODO()
 	}
-	opt.WithLogger(nil)
-	db, err := badger.Open(opt)
-	return &Client{db: db, ttl: option.Ttl}, err
+	context, cancel := context.WithCancel(ctx)
+	client := &Client{
+		ttl: int64(option.TTL),
+		ctx: context,
+		cnl: cancel,
+	}
+	if client.ttl > 0 {
+		go client.run()
+	}
+	return client
 }
 
+type rawData struct {
+	data []byte
+	time int64
+}
 type Client struct {
-	db  *badger.DB
-	ttl time.Duration
-}
-type SetOption struct {
-	Ttl time.Duration
+	ttl  int64
+	data sync.Map
+	ctx  context.Context
+	cnl  context.CancelFunc
 }
 
-func (obj *Client) Set(key []byte, data any, options ...SetOption) error {
-	var option SetOption
-	if len(options) > 0 {
-		option = options[0]
-	}
-	var val []byte
-	switch con := data.(type) {
-	case []byte:
-		val = con
-	case string:
-		val = tools.StringToBytes(con)
-	default:
-		b := bytes.NewBuffer(nil)
-		if err := gob.NewEncoder(b).Encode(con); err != nil {
-			return err
+func (obj *Client) run() {
+	for {
+		select {
+		case <-obj.ctx.Done():
+			return
+		default:
+			obj.data.Range(func(key, value any) bool {
+				val := value.(rawData)
+				if time.Now().Unix()-val.time > obj.ttl {
+					obj.data.Delete(key)
+				}
+				return true
+			})
+			select {
+			case <-obj.ctx.Done():
+				return
+			case <-time.After(time.Second * 30):
+			}
 		}
-		val = b.Bytes()
 	}
-	if option.Ttl == 0 {
-		option.Ttl = obj.ttl
-	}
-	return obj.db.Update(func(txn *badger.Txn) error {
-		if option.Ttl > 0 {
-			return txn.SetEntry(badger.NewEntry(key, val).WithTTL(option.Ttl))
-		} else {
-			return txn.SetEntry(badger.NewEntry(key, val))
-		}
+}
+
+func (obj *Client) set(key string, data []byte) {
+	obj.data.Store(key, rawData{
+		data: data,
+		time: time.Now().Unix(),
 	})
 }
 
-func (obj *Client) Get(key []byte, t ...any) (val []byte, err error) {
-	err = obj.db.View(func(txn *badger.Txn) error {
-		item, err := txn.Get(key)
-		if err != nil {
-			return err
-		}
-		val, err = item.ValueCopy(nil)
-		return err
-	})
-	if err == nil && len(t) > 0 {
-		err = gob.NewDecoder(bytes.NewBuffer(val)).Decode(t[0])
+func (obj *Client) get(key string) ([]byte, bool) {
+	val, ok := obj.data.Load(key)
+	if !ok {
+		return nil, false
 	}
-	return
+	return val.(rawData).data, true
 }
-func (obj *Client) Close() error {
-	return obj.db.Close()
+func (obj *Client) Set(key string, data []byte) {
+	obj.set(key, data)
+}
+
+func (obj *Client) Get(key string) ([]byte, bool) {
+	return obj.get(key)
+}
+func (obj *Client) Close() {
+	obj.cnl()
+	obj.data.Clear()
 }
--- go.mod
+++ go.mod
@@ -1,32 +1,3 @@
 module github.com/gospider007/db
 
-go 1.21.3
-
-require (
-	github.com/dgraph-io/badger/v4 v4.2.0
-	github.com/gospider007/tools v0.0.0-20231202084937-8b2bc66f8198
-)
-
-require (
-	github.com/andybalholm/brotli v1.0.6 // indirect
-	github.com/cespare/xxhash/v2 v2.2.0 // indirect
-	github.com/dgraph-io/ristretto v0.1.1 // indirect
-	github.com/dustin/go-humanize v1.0.1 // indirect
-	github.com/gogo/protobuf v1.3.2 // indirect
-	github.com/golang/glog v1.2.0 // indirect
-	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
-	github.com/golang/protobuf v1.5.3 // indirect
-	github.com/golang/snappy v0.0.4 // indirect
-	github.com/google/flatbuffers v23.5.26+incompatible // indirect
-	github.com/gospider007/kinds v0.0.0-20231024093643-7a4424f2d30e // indirect
-	github.com/gospider007/re v0.0.0-20231024115818-adfd03636256 // indirect
-	github.com/klauspost/compress v1.17.4 // indirect
-	github.com/pkg/errors v0.9.1 // indirect
-	github.com/stretchr/testify v1.8.4 // indirect
-	go.opencensus.io v0.24.0 // indirect
-	golang.org/x/image v0.14.0 // indirect
-	golang.org/x/net v0.19.0 // indirect
-	golang.org/x/sys v0.15.0 // indirect
-	golang.org/x/text v0.14.0 // indirect
-	google.golang.org/protobuf v1.31.0 // indirect
-)
+go 1.23.0
