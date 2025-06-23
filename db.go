42f6de1d7af014d2c89f249b9fcb52b63319f40a
gospider <2216403312@qq.com>
2025-06-23 17:23:23 +0800

    sync

--- db.go
+++ db.go
@@ -1,16 +1,28 @@
 package db
 
 import (
+	"bytes"
 	"context"
+	"embed"
+	"encoding/gob"
+	"os"
 	"sync"
 	"time"
+
+	"github.com/gospider007/tools"
 )
 
+type FS struct {
+	FS  *embed.FS
+	Dir string
+}
 type ClientOption struct {
 	TTL time.Duration
+	Dir string
+	FS  FS
 }
 
-func NewClient(ctx context.Context, option ClientOption) *Client {
+func NewClient(ctx context.Context, option ClientOption) (*Client, error) {
 	if ctx == nil {
 		ctx = context.TODO()
 	}
@@ -19,11 +31,20 @@ func NewClient(ctx context.Context, option ClientOption) *Client {
 		ttl: int64(option.TTL),
 		ctx: context,
 		cnl: cancel,
+		dir: option.Dir,
+		fs:  option.FS,
 	}
-	if client.ttl > 0 {
+
+	if client.dir != "" && !tools.PathExist(client.dir) {
+		err := os.MkdirAll(client.dir, 0777)
+		if err != nil {
+			return nil, err
+		}
+	}
+	if client.ttl > 0 && client.dir != "" {
 		go client.run()
 	}
-	return client
+	return client, nil
 }
 
 type rawData struct {
@@ -35,6 +56,8 @@ type Client struct {
 	data sync.Map
 	ctx  context.Context
 	cnl  context.CancelFunc
+	dir  string
+	fs   FS
 }
 
 func (obj *Client) run() {
@@ -73,12 +96,40 @@ func (obj *Client) get(key string) ([]byte, bool) {
 	}
 	return val.(rawData).data, true
 }
-func (obj *Client) Set(key string, data []byte) {
-	obj.set(key, data)
+func (obj *Client) Set(key string, data any) error {
+	key = tools.Hex(tools.Md5(key))
+	b := bytes.NewBuffer(nil)
+	err := gob.NewEncoder(b).Encode(data)
+	if err != nil {
+		return err
+	}
+	if obj.dir != "" {
+		return os.WriteFile(tools.PathJoin(obj.dir, key), b.Bytes(), 0777)
+	} else {
+		obj.set(key, b.Bytes())
+	}
+	return nil
 }
 
-func (obj *Client) Get(key string) ([]byte, bool) {
-	return obj.get(key)
+func (obj *Client) Get(key string, data any) (bool, error) {
+	key = tools.Hex(tools.Md5(key))
+	if obj.dir != "" && !tools.PathExist(tools.PathJoin(obj.dir, key)) {
+		b, err := os.ReadFile(tools.PathJoin(obj.dir, key))
+		if err == nil {
+			return true, gob.NewDecoder(bytes.NewBuffer(b)).Decode(data)
+		}
+	}
+	if obj.fs.FS != nil && obj.fs.Dir != "" {
+		b, err := obj.fs.FS.ReadFile(obj.fs.Dir + "/" + key)
+		if err == nil {
+			return true, gob.NewDecoder(bytes.NewBuffer(b)).Decode(data)
+		}
+	}
+	b, ok := obj.get(key)
+	if ok {
+		return true, gob.NewDecoder(bytes.NewBuffer(b)).Decode(data)
+	}
+	return false, nil
 }
 func (obj *Client) Close() {
 	obj.cnl()
